932b4337a6f7bdb40a62b4495e2338bad9efe042
thomas-hackett <145539934+thomas-hackett@users.noreply.github.com>
2025-03-12 09:09:33 +0100

    demo on moving

--- snake.py
+++ snake.py
@@ -1,9 +1,10 @@
 import os
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
+
 import random
 import pygame.display
 import pgzrun
-
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -12,27 +13,46 @@ pygame.display.set_caption('backgroun image example')
 
 background = pygame.image.load('images/snakes.jpg')
 
-player = Actor("player", (675, 795))
+blue = Actor("blue", (422, 830))
+
+sq = [422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,
+      422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,]
+counter = 0
 
 def draw():
     screen.blit(background, (385, 120))
-    player.draw()
+    blue.draw()
 
 def on_key_down(key):
+    global counter
     if key == keys.F:
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
     elif key == keys.W:
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT))
     elif key == keys.L:
         pgzrun.quit()
+    elif key == keys.O:
+        blue.x = sq7
+    if key == keys.SPACE:
+        dice = random.randint(1, 6)
+        counter += dice
+        blue.x = sq[counter]
+        if 11 <= counter <= 20:
+            blue.y = 755
+        elif 21 <= counter <= 30:
+            blue.y = 680
+        elif 31 <= counter <= 40:
+            blue.y = 605
 
 def update():
     # Move the player with arrow keys
     if keyboard.left:
-        player.x = player.x - 1
+        blue.x = blue.x - 1
     elif keyboard.right:
-        player.x = player.x + 1
+        blue.x = blue.x + 1
 
-# def update():
+
 
 pgzrun.go()
