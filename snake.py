0fce6fc50e3aea5a32fe191140b79e41468152f4
thomas-hackett <145539934+thomas-hackett@users.noreply.github.com>
2025-03-13 08:51:03 +0100

    morning update

--- snake.py
+++ snake.py
@@ -13,20 +13,67 @@ pygame.display.set_caption('backgroun image example')
 
 background = pygame.image.load('images/snakes.jpg')
 
+one = pygame.image.load('images/dice-six-faces-one.png')
+two = pygame.image.load('images/dice-six-faces-two.png')
+three = pygame.image.load('images/dice-six-faces-three.png')
+four = pygame.image.load('images/dice-six-faces-four.png')
+five = pygame.image.load('images/dice-six-faces-five.png')
+six = pygame.image.load('images/dice-six-faces-six.png')
+
 blue = Actor("blue", (422, 830))
+red = Actor("red", (422, 830))
 
 sq = [422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
       1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,
       422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
-      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,]
-counter = 0
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,
+      422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,
+      422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422,
+      422, 497, 572, 647, 722, 797, 872, 947, 1022, 1097,
+      1097, 1022, 947, 872, 797, 722, 647, 572, 497, 422]
+counterblue = 0
+counterred = 0
+
+bluepl = True
+
+funnytext = "boo"
+
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 
 def draw():
     screen.blit(background, (385, 120))
     blue.draw()
+    red.draw()
+
+    text_surface = my_font.render(funnytext, False, (255, 255, 255))
+    screen.blit(text_surface, (0, 0))
+
+def move_blue():
+    global counterblue
+    if 10 <= counterblue <= 19:
+        blue.y = 755
+    elif 20 <= counterblue <= 29:
+        blue.y = 680
+    elif 30 <= counterblue <= 39:
+        blue.y = 605
+    elif 40 <= counterblue <= 49:
+        blue.y = 530
+    elif 50 <= counterblue <= 59:
+        blue.y = 455
+    elif 60 <= counterblue <= 69:
+        blue.y = 380
+    elif 70 <= counterblue <= 79:
+        blue.y = 305
+    elif 80 <= counterblue <= 89:
+        blue.y = 230
+    elif 90 <= counterblue <= 100:
+        blue.y = 155
 
 def on_key_down(key):
-    global counter
+    global counterblue, counterred, bluepl, funnytext
     if key == keys.F:
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
     elif key == keys.W:
@@ -35,16 +82,87 @@ def on_key_down(key):
         pgzrun.quit()
     elif key == keys.O:
         blue.x = sq7
+    elif key == keys.P:
+        blue.x = sq[0]
+        blue.y = 830
+        counterblue = 0
+
+        red.x = sq[0]
+        red.y = 830
+        counterred = 0
+
+        funnytext = "nuh uh"
+
     if key == keys.SPACE:
-        dice = random.randint(1, 6)
-        counter += dice
-        blue.x = sq[counter]
-        if 11 <= counter <= 20:
-            blue.y = 755
-        elif 21 <= counter <= 30:
-            blue.y = 680
-        elif 31 <= counter <= 40:
-            blue.y = 605
+        if bluepl:
+            dice = random.randint(1, 6)
+            counterblue += dice
+            if counterblue > 99:
+                counterblue = 99
+
+            blue.x = sq[counterblue]
+
+            move_blue()
+
+            if counterblue == 3:
+                counterblue = 13
+                blue.x = sq[counterblue]
+
+            if dice == 1:
+                screen.blit(one, (10, 200))
+            elif dice == 2:
+                screen.blit(two, (10, 200))
+            elif dice == 3:
+                screen.blit(three, (10, 200))
+            elif dice == 4:
+                screen.blit(four, (10, 200))
+            elif dice == 5:
+                screen.blit(five, (10, 200))
+            elif dice == 6:
+                screen.blit(six, (10, 200))
+
+            bluepl = False
+
+        else :
+            dice = random.randint(1, 6)
+            counterred += dice
+            if counterred > 99:
+                counterred = 99
+
+            red.x = sq[counterred]
+
+            if 10 <= counterred <= 19:
+                red.y = 755
+            elif 20 <= counterred <= 29:
+                red.y = 680
+            elif 30 <= counterred <= 39:
+                red.y = 605
+            elif 40 <= counterred <= 49:
+                red.y = 530
+            elif 50 <= counterred <= 59:
+                red.y = 455
+            elif 60 <= counterred <= 69:
+                red.y = 380
+            elif 70 <= counterred <= 79:
+                red.y = 305
+            elif 80 <= counterred <= 89:
+                red.y = 230
+            elif 90 <= counterred <= 100:
+                red.y = 155
+
+            if dice == 1:
+                screen.blit(one, (10, 200))
+            elif dice == 2:
+                screen.blit(two, (10, 200))
+            elif dice == 3:
+                screen.blit(three, (10, 200))
+            elif dice == 4:
+                screen.blit(four, (10, 200))
+            elif dice == 5:
+                screen.blit(five, (10, 200))
+            elif dice == 6:
+                screen.blit(six, (10, 200))
+            bluepl = True
 
 def update():
     # Move the player with arrow keys
